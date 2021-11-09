4a2e3d18fd594d2cea7ab9eb5f92303d32292d0c
rijalislamir <rijalislamir@gmail.com>
2021-11-09 15:34:55 +0700

    Add context when render view with dummy data

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        "author": "Rijal Islami",
+        "title": "Blog post 1",
+        "content": "First post content",
+        "date": "August 27, 2021"
+    },
+    {
+        "author": "John Doe",
+        "title": "Blog post 2",
+        "content": "Second post content",
+        "date": "August 28, 2021"
+    }
+]
 
 def home(request):
-    return render(request, "blog/home.html")
+    context = {
+        "posts": posts
+    }
+    return render(request, "blog/home.html", context)
 
 def about(request):
     return render(request, "blog/about.html")
